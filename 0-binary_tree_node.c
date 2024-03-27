9f6b0ea7545a8f5b4b3f6aa23fd683a4a39c146f
leonard-Nfor <leonardnforkort@gmail.com>
2024-03-27 06:29:01 -0700

    m

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_node - the function name
+ * @parent: aa pointer to the parent of the node to create
+ * @value: The data to insert
+ * Return: Null if an errot occur else newnode created
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if newnode == NULL
+		return (NULL);
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	return (newnode);
+}
+
