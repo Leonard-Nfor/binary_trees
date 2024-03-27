ffcabd6af570654ab5a37b63711c00d60fd8fcdd
leonard-Nfor <leonardnforkort@gmail.com>
2024-03-27 12:28:31 -0700

    m

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - functions name.
+ * @tree: A pointer to the root node of the tree to count the number of nodes.
+ *
+ * Return: If tree is NULL, the function must return 0, else return node count.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree)
+	{
+		/*if the condition is yes add 1 else add 0 */
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
+}
